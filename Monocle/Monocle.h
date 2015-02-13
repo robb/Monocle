15af4eb2078474400fdd3ffcf925946d3d03c7d4
Robert Böhnke <robb@robb.is>
2015-02-13 12:52:51 +0100

    Initial commit

---
+++ Monocle/Monocle.h
@@ -0,0 +1,13 @@
+//
+//  Monocle.h
+//  Monocle
+//
+//  Created by Robert Böhnke on 13/02/15.
+//  Copyright (c) 2015 Robert Böhnke. All rights reserved.
+//
+
+//! Project version number for Monocle.
+extern double MonocleVersionNumber;
+
+//! Project version string for Monocle.
+extern const unsigned char MonocleVersionString[];
---
+++ Specs/Specs.swift
@@ -0,0 +1,36 @@
+//
+//  Specs.swift
+//  Specs
+//
+//  Created by Robert Böhnke on 13/02/15.
+//  Copyright (c) 2015 Robert Böhnke. All rights reserved.
+//
+
+import Monocle
+import XCTest
+
+class Specs: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
